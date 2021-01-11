e362a0d1909430520d7b1be54ba4f54bef0e7905
Keaf <keaf@chromium.org>
2021-01-11 01:07:59 +0000

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ERPLoading
+{
+    public class Main : MelonLoader.MelonMod
+    {
+        private AssetBundle bl { get; set; }
+        public override void OnApplicationStart()
+        {
+            bl = AssetBundle.LoadFromMemory(Properties.Resources.erp);
+        }
+        public override void OnLevelWasLoaded(int level)
+        {
+            try
+            {
+                GameObject.DestroyImmediate(GameObject.Find("/UserInterface/MenuContent/Screens/Title/LogoContainer/vrchatlogo2sided"));
+                GameObject.Instantiate(bl.LoadAsset<GameObject>("ERP ANimation Variant"), GameObject.Find("/UserInterface/MenuContent/Screens/Title/LogoContainer").transform);
+            }
+            catch { }
+        }
+    }
+}
